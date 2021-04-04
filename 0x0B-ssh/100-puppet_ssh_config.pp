a53efa51b51d3865771036e9849b90f7b95b2a27
mehdizit <zitounimehdi.it@gmail.com>
2021-04-04 23:03:54 +0100

    up

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
-#connect to a server without typing a password
+#Turn off password config
 file_line { 'Turn off passwd auth':
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
+    path  => '/etc/ssh/ssh_config',
+    line  => '     PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
-  path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/holberton',
+    path  => '/etc/ssh/ssh_config',
+    line  => '     IdentityFile ~/.ssh/holberton',
 }
