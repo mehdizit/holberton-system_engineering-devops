1f903f9c31996aec17f988f84922dc4a315245d7
mehdizit <zitounimehdi.it@gmail.com>
2021-05-03 21:48:07 +0100

    up

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""gathers information about an employee by ID"""
+import json
+from collections import OrderedDict
+import requests
+import sys
+
+
+def get_user_todo():
+    '''
+    get user todo
+    '''
+    emp = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                       .format(sys.argv[1]))
+    name = emp.json().get('username')
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                         .format(sys.argv[1]))
+    tasks = tasks.json()
+    res = []
+    final_id = OrderedDict()
+    filename = sys.argv[1] + ".json"
+
+    with open(filename, 'w+') as f:
+        for task in tasks:
+            final = OrderedDict()
+            final['task'] = task['title']
+            final['completed'] = task['completed']
+            final['username'] = name
+            res.append(final)
+        final_id[sys.argv[1]] = res
+        json.dump(final_id, f)
+
+
+if __name__ == "__main__":
+    get_user_todo()
---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""gathers information about an employee by ID"""
+import json
+from collections import OrderedDict
+import requests
+import sys
+
+
+def get_users_todo():
+
+    emp = requests.get('https://jsonplaceholder.typicode.com/users')
+    users = {}
+    final_id = OrderedDict()
+    filename = "todo_all_employees.json"
+
+    for item in emp.json():
+        users[item['id']] = item['username']
+
+    with open(filename, 'w+') as f:
+        for user in users:
+            tasks = requests.get(
+                'https://jsonplaceholder.typicode.com/todos?userId={}'
+                .format(user))
+            tasks = tasks.json()
+            res = []
+            for task in tasks:
+                final = OrderedDict()
+                final['username'] = users[user]
+                final['task'] = task['title']
+                final['completed'] = task['completed']
+                res.append(final)
+            final_id[user] = res
+        json.dump(final_id, f)
+
+
+if __name__ == "__main__":
+    get_users_todo()
